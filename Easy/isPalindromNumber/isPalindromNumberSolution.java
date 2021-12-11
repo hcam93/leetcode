7fa577dcdbbfeabf4d6582408e51cdc9fcc8ca34
Hunter Camfield <camfieldhunter@gmail.com>
2021-12-10 16:31:49 -0800

    Add isNumberPalindrom

---
+++ Easy/isPalindromNumber/isPalindromNumberSolution.java
@@ -0,0 +1,27 @@
+package Easy.isPalindromNumber;
+
+public class isPalindromNumberSolution {
+    
+
+    public static boolean isPalindrome(int x){
+        if(x < 0) return false;
+        if(x / 10 == 0) return true;
+        String stringVal = String.valueOf(x);
+        int pointerOne = 0;
+        int pointerTwo = stringVal.length() - 1;
+        while(pointerOne < pointerTwo){
+            if(stringVal.charAt(pointerOne) == stringVal.charAt(pointerTwo)){
+                pointerOne++;
+                pointerTwo--;
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(10));
+    }
+}
